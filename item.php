0e04adfb40d50dbee3d21d4897db7b63f76c196d
StevenJ <s.jespers@telenet.be>
2016-12-14 14:25:45 +0100

    bezig aan itemfull en wat css aanpassingen

--- item.php
+++ item.php
@@ -11,6 +11,12 @@ use OWG\Weggeefwinkel\Business\UserService;
 use OWG\Weggeefwinkel\Entities\Item;
 use OWG\Weggeefwinkel\Business\PhotoService;
 
+if (!isset($_SESSION["username"])) {
+
+    header("location: login.php");
+    exit(0);
+}
+
 $sectionSvc = new SectionService();
 $sectionList = $sectionSvc->getAll();
 $itemSvc = new ItemService();
@@ -20,59 +26,56 @@ if (isset($_GET["id"])) {
 }
 
 if (isset($_GET["action"])) {
-    if (!isset($_SESSION["username"])) {
-
-        header("location: login.php");
-        exit(0);
-    } else {
-        if ($_GET["action"] == "add") {
-            if (isset($_POST["addItem"])) {
-                $photoSvc = new PhotoService();
-                $photoName = $photoSvc->handlePhoto($_FILES["img"]);
-                $user = $userSvc->getByUsername($_SESSION["username"]);
-                $itemSvc->addItem($_POST["title"], $_POST["description"], $photoName, $_POST["section"], $user->getId());
-                header("location: account.php");
-                exit(0);
-            } else {
-                $view = $twig->render("addItem.twig", array("sectionList" => $sectionList, "username" => $_SESSION["username"]));
-                print($view);
-            }
-        }
-
-        if ($item->getUser()->getUsername() == $_SESSION["username"]) {
-            //print_r($item);
-            if ($_GET["action"] == "edit") {
-
-
-                if (isset($_POST["submit"])) {
-                    $photoSvc = new PhotoService();
-                    //print_r($_FILES["img"]);
-                    $photoName = $photoSvc->handlePhoto($_FILES["img"]);
-                    $itemSvc->updateItem($_GET["id"], $_POST["title"], $_POST["description"], $photoName, $_POST["sectionId"]);
-                    /* $itemSvc = new ItemService();
-                      $item = $itemSvc->getById($_GET["id"]); */
-                    header("location: account.php");
-                    exit(0);
-                    //print_r($item);
-                    //$itemUser = $itemSvc->getUser($_GET["id"]);
-                }
-
-                $view = $twig->render("editItem.twig", array("item" => $item, "sectionList" => $sectionList, "username" => $_SESSION["username"]));
-                print($view);
-            }
-
-            if ($_GET["action"] == "delete") {
-                $itemSvc->deleteItem($_GET["id"]);
-                header("location: account.php");
-            }
+    if ($_GET["action"] == "add") {
+        if (isset($_POST["addItem"])) {
+            $photoSvc = new PhotoService();
+            $photoName = $photoSvc->handlePhoto($_FILES["img"]);
+            $user = $userSvc->getByUsername($_SESSION["username"]);
+            $itemSvc->addItem($_POST["title"], $_POST["description"], $photoName, $_POST["section"], $user->getId());
+            header("location: account.php");
+            exit(0);
         } else {
-            print "ni van u eh";
+            $view = $twig->render("addItem.twig", array("sectionList" => $sectionList, "username" => $_SESSION["username"]));
+            print($view);
         }
     }
 
 
+    //print_r($item);
+    elseif ($_GET["action"] == "edit") {
+
+
+        if (isset($_POST["submit"])) {
+            if ($item->getUser()->getUsername() == $_SESSION["username"]) {
+                $photoSvc = new PhotoService();
+                //print_r($_FILES["img"]);
+                $photoName = $photoSvc->handlePhoto($_FILES["img"]);
+                $itemSvc->updateItem($_GET["id"], $_POST["title"], $_POST["description"], $photoName, $_POST["sectionId"]);
+                /* $itemSvc = new ItemService();
+                  $item = $itemSvc->getById($_GET["id"]); */
+                header("location: items.php");
+                exit(0);
+                //print_r($item);
+                //$itemUser = $itemSvc->getUser($_GET["id"]);
+            } else {
+                print "ni van u eh";
+            }
+           
+        }
+         $view = $twig->render("editItem.twig", array("item" => $item, "sectionList" => $sectionList, "username" => $_SESSION["username"]));
+        print($view);
+    } elseif ($_GET["action"] == "delete") {
+        $itemSvc->deleteItem($_GET["id"]);
+        header("location: account.php");
+    } elseif ($_GET["action"] == "show") {
+        $view = $twig->render("showItem.twig", array("item" => $item, "sectionList" => $sectionList, "username" => $_SESSION["username"]));
+        print($view);
+    }
+}
+
+
 //print "jeuj: " . $_SESSION["username"];
-} else {
+else {
     $view = $twig->render("showItem.twig", array("item" => $item, "username" => $_SESSION["username"]));
     print($view);
 }
--- items.php
+++ items.php
@@ -1,8 +1,28 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
+require_once 'bootstrap.php';
+
+
+use OWG\Weggeefwinkel\Business\ItemService;
+use OWG\Weggeefwinkel\Business\UserService;
+use OWG\Weggeefwinkel\Business\CityService;
+
+if (!isset($_SESSION["username"])) {
+    header("location: login.php");
+    exit(0);
+} else {
+    $itemSvc = new ItemService();
+    $itemList = $itemSvc->getByUser($_SESSION["username"]);
+    $userSvc = new UserService();
+    
+    //$citySvc = new CityService();
+    //$cityList = $citySvc->getAll();
+    
+    $user = $userSvc->getByUsername($_SESSION["username"]);
+    
+    //print_r($user);
+    $view = $twig->render("items.twig", array("itemList" => $itemList, "username" =>$_SESSION["username"], "user" => $user));
+    print($view);
+}
 
